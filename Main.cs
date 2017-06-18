944ebaa2b25ec8d2934fd42ffac56cd99a12f1da
test <Test>
2017-06-18 15:24:52 +0300

    Add Classes

--- Main.cs
+++ Main.cs
@@ -7,7 +7,15 @@ namespace NS {
 	public Node leftNode;
 	public Node rightNode;
 	public int valueI;
+	public int eval(){return valueI;}
 	}
 
+ class DelitNode : Node{
+	override int eval(){return leftNode.eval()/rightNode.eval();}
+	}
+
+ class UmnozNode : Node{
+	override int eval(){return leftNode.eval()*rightNode.eval();}
+	}
 
 }
