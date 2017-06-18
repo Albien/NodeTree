9c843fe54ee8bb95c36ebc9d549fd10fbfd18fe7
test <Test>
2017-06-18 15:02:30 +0300

    Commit2

--- Main.cs
+++ Main.cs
@@ -3,15 +3,18 @@ using System.IO;
 
 namespace NS {
 
-class Node{
-Node leftNode;
-Node rightNode;
-int valueI;
-int eval(){
-if(valueI != null){
-return valueI
-}else{return eval()}
-}
+ class Node{
+	char oper;
+	Node leftNode;
+	Node rightNode;
+	int valueI;
+	int eval(){
+
+	if(valueI != null){
+	return valueI
+	}else{return eval()}
+
+	}
 }
 
-
+}
